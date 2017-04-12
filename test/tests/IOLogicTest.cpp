7bb8e5daf51bc52503d79f72973de57cd6066da1
Maximilian Beck <akkumulator1@gmail.com>
2017-04-12 15:26:06 +0200

    test: started new tests

---
+++ test/tests/IOLogicTest.cpp
@@ -0,0 +1,14 @@
+/*
+#include "gtest/gtest.h"
+#include "IOLogic.h"
+
+
+namespace {
+IOLogic iol;
+
+// somehow expect pin to be set
+TEST(IOLogicTest, setOutputTriggersSetPin) {
+    iol.setOutput(8, 0);
+}
+}
+*/
